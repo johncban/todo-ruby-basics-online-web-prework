56ca08fc23e198c9f4c8955c29e5a2c7f58dff30
johncban <johncban@gmail.com>
2019-04-10 07:28:46 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -14,9 +14,8 @@ def argue(argueOne="I am right", argueTwo="you are wrong!")
 end
 
 def greeting (greeting = "Hi there, ", name = "Bobby!")
-  greet = "#{greeing} #{name}"
   puts "#{greeting}, #{name}"
-  return greet
+  return greeting name
 end
 
 def return_a_value
