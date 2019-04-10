60a8cf16404567af1464edcd19b482cdf73ff7e3
johncban <johncban@gmail.com>
2019-04-10 06:28:42 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -26,7 +26,7 @@ def last_evaluated_value
   return "expert"
 end
 
-def pizza_party(cheese, toppings = "pepperoni")
+def pizza_party(cheese, toppings == "pepperoni")
   puts "Make pizza with #{cheese} and #{toppings}"
-  return cheese, toppings 
+  return cheese, toppings
 end
